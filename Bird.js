d680f1921d06fc1f05708ef2a7a95b104e756d18
apurvamahale <73888034+apurvamahale@users.noreply.github.com>
2021-02-08 17:57:18 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,20 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage=loadImage("sprites/smoke.png");
+    this.trajectory=[];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    var position=[this.body.position.x,this.body.position.y];
+    this.trajectory.push(position);
+    for(var I=0;I < this.trajectory.length;I++){
+      image(this.smokeImage,this.trajectory[I][0],this.trajectory[I][1]);
+    }
+  }
+}
